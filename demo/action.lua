0ebaeb86c4339ade19d410f46e030a961d3a7b44
Fabian Esteban Beltran Gomez <fabianbeltran1224@gmail.com>
2025-02-09 11:29:21 -0500

    adding .gitignore file

--- demo/action.lua
+++ demo/action.lua
@@ -1,23 +1,25 @@
 function action_init()
-	action_state="enable" -- enable or disable
-	action_time=30
-	action_clock=0
+	action = {
+		state = "enable", -- enable or disable
+		time = 30,
+		clock = 0
+	}
 end
 
 function action_update()
-	if(action_state=="disable")then
-		action_clock+=1
+	if (action.state == "disable") then
+		action.clock += 1
 	end
-	if(action_clock==30)then
-		action_clock=0
-		action_state="enable"
+	if (action.clock == 30) then
+		action.clock = 0
+		action.state = "enable"
 	end
 end
 
 function action_draw()
- if action_state=="enable" then
-		rect(0,0,127,127,11)
+	if action.state == "enable" then
+		rect(0, 0, 127, 127, 11)
 	else
-		rect(0,0,127,127,5)	
+		rect(0, 0, 127, 127, 5)
 	end
 end
--- demo/main.lua
+++ demo/main.lua
@@ -1,46 +1,45 @@
 function _init()
-	poke(0x5f2d,1)
+	poke(0x5f2d, 1)
 	action_init()
 	mana_init()
 	building_init()
 	store_init()
-	
-	
-	clock= 0 
+
+	clock = 0
 	secs = 0
- start_flag=false
+	start_flag = false
 end
 
 function _update()
- mana_update()
- building_update()
- store_update()
- action_update()
- 
- if(start_flag)then
-		if(clock>=30)then
-			secs+=1
-			clock=0
+	mana_update()
+	building_update()
+	store_update()
+	action_update()
+
+	if start_flag then
+		if (clock >= 30) then
+			secs += 1
+			clock = 0
 		end
 	end
-	
-	clock+=1
-	
-	if(mana.quantity>=100)then
-		start_flag=false
-	end	
+
+	clock += 1
+
+	if (mana.quantity >= 100) then
+		start_flag = false
+	end
 end
 
 function _draw()
 	cls()
 	map()
-	print("segundos: "..secs,64,2)
-	
+	print("segundos: " .. secs, 64, 2)
+
 	mana_draw()
 	building_draw()
 	store_draw()
 	action_draw()
 	printh("test", "log")
---	print(stat(32).."/"..stat(33),stat(32),stat(33))
-	circfill(stat(32),stat(33),1,6)
+	--	print(stat(32).."/"..stat(33),stat(32),stat(33))
+	circfill(stat(32), stat(33), 1, 6)
 end
